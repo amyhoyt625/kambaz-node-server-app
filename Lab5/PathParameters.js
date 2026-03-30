38eb62d243a9bdf36793ab2848189098f97ab9c1
amyhoyt625 <amylenahoyt@gmail.com>
2026-03-29 22:13:48 -0400

    a5 node

--- Lab5/PathParameters.js
+++ Lab5/PathParameters.js
@@ -1,15 +1,23 @@
 export default function PathParameters(app) {
-    const add = (req, res) => {
-      const { a, b } = req.params;
-      const sum = parseInt(a) + parseInt(b);
-      res.send(sum.toString());
-    };
-    const substract = (req, res) => {
-      const { a, b } = req.params;
-      const sum = parseInt(a) - parseInt(b);
-      res.send(sum.toString());
-    };
-    app.get("/lab5/add/:a/:b", add);
-    app.get("/lab5/subtract/:a/:b", substract);
-   };
-   
+  const add = (req, res) => {
+    const { a, b } = req.params;
+    res.send((parseInt(a) + parseInt(b)).toString());
+  };
+  const subtract = (req, res) => {
+    const { a, b } = req.params;
+    res.send((parseInt(a) - parseInt(b)).toString());
+  };
+  const multiply = (req, res) => {
+    const { a, b } = req.params;
+    res.send((parseInt(a) * parseInt(b)).toString());
+  };
+  const divide = (req, res) => {
+    const { a, b } = req.params;
+    res.send((parseInt(a) / parseInt(b)).toString());
+  };
+
+  app.get("/lab5/add/:a/:b", add);
+  app.get("/lab5/subtract/:a/:b", subtract);
+  app.get("/lab5/multiply/:a/:b", multiply);
+  app.get("/lab5/divide/:a/:b", divide);
+}
